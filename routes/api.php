9e19d1111ade9a5009040669cc86151903c01161
Rangga Adithia <rangga.adithia26@gmail.com>
2025-05-08 10:23:24 +0800

    add: route-login

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,20 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users', function (Request $request) {
     $users = \App\Models\User::all();
     return response()->json($users);
+});
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        return response()->json(['message' => 'Login successful', 'authenticate' => true, 'user' => Auth::user(),]);
+    }
+
+    return response()->json(['message' => 'Unauthorize', 'authenticate' => false], 403);
 });
